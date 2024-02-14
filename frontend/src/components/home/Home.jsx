bb52a62902eb2bf330e27b3bd634c42ad61ec2d3
Panda0196 <mike.wazowski0196@gmail.com>
2024-02-14 15:20:41 -0800

    Update frontend for card

--- frontend/src/components/home/Home.jsx
+++ frontend/src/components/home/Home.jsx
@@ -17,6 +17,7 @@ const Home = () => {
   const [uploadTrue, setUploadTrue] = useState(false)
   const [card, setCard] = useState([])
   console.log("card>>>", card)
+  const [lengthOfDiv,setLenghtofDiv] = useState()
   const [searchInput, setSearchInput] = useState('')
   const [inputMessage, setInputMessage] = useState('');
   const [message, setMessage] = useState([])
@@ -87,7 +88,7 @@ const Home = () => {
           setChatRoomId(AllChat.data.userChat[0]._id)
           setMessage(AllChat.data.userChat[0].messages)
 
-          const messageCard = AllChat.data.userChat[0].messages
+          const messageCard = AllChat.data.userChat[0].cards
 
           const newCardList = [];
           for (let i = 0; i < messageCard.length - 1; i += 2) {
@@ -128,7 +129,7 @@ const Home = () => {
           if (Chat.data.sucess == true) {
             setChatRoomId(Chat.data.userChat._id)
             setMessage(Chat.data.userChat.messages)
-            const messageCard = Chat.data.userChat.messages
+            const messageCard = Chat.data.userChat.cards
 
             const newCardList = [];
             for (let i = 0; i < messageCard.length - 1; i += 2) {
@@ -169,6 +170,13 @@ const Home = () => {
 
     setMessage(prev => ([...prev, userMesseage]))
     setInputMessage('')
+
+    // const msgOuterDiv = document.getElementById("msg_outer");
+    // const lastDiv = msgOuterDiv?.lastElementChild;
+    // console.log("divetofocus", lastDiv)
+    // lastDiv?.focus();
+    // lastDiv?.scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest", scrollOffset: "50px" });
+
     try {
       let body;
 
@@ -210,6 +218,77 @@ const Home = () => {
     }
   }
 
+  const handleKeyPress = (event) => {
+    if (event.key === "Enter") {
+      handleSend();
+    }
+  };
+
+
+  // useEffect(() => {
+
+  //   const msgOuterDiv = document.getElementById("msg_outer");
+  //   const lastDiv = msgOuterDiv?.lastElementChild;
+  //   console.log("divetofocus", lastDiv)
+  //   lastDiv?.focus();
+  //   lastDiv?.scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest", scrollOffset: "50px" });
+
+  // }, [lengthOfDiv])
+
+
+  // useEffect(() => {
+  //   const chatLog = document.getElementById("msg_outer");
+  //   const lengthOfDiv = chatLog.children.length - 1;
+  //   if (lengthOfDiv > 0) {
+  //     const lastMsgDiv = chatLog.children[lengthOfDiv - 1];
+  //     setLenghtofDiv(lastMsgDiv.innerText);
+  //   }
+  // }, [message])
+
+
+
+
+  const handleDeleteHistory = async(e)=>{
+    e.preventDefault();
+
+    const isConfirmed = window.confirm("Are you sure you want to delete the chat history?");
+    if (isConfirmed) {
+    try {
+      const deleteChat =await axios.delete(`${Base_Url}/api/deleteHistory/${chatRoomId}`)
+      console.log("deleteChat>>>>>",deleteChat)
+      toast.success(deleteChat.data.message)
+      if (deleteChat.data.success === true) {
+        setTimeout(() => {
+          window.location.reload()
+        }, 3000)
+      }
+      
+    } catch (error) {
+      console.log("error>>>>",error)
+      toast.error("something went wrong !")
+    }
+  }
+  }
+
+
+const handleDeleteDocs = async(e) => {
+  e.preventDefault();
+
+  try {
+    const deleteDocs = await axios.delete(`${Base_Url}/api/delete-docs`);
+    console.log("deleteDocs>>>>>>",deleteDocs)
+    toast.success("docs deleted successFully")
+    
+  } catch (error) {
+    console.log("error>>>>>>>>",error)
+    toast.error("something went wrong !")
+  }
+
+}
+
+
+
+
 
 
   const filterCard = searchInput ? card.filter((value) => value.question.toLowerCase().includes(searchInput.toLowerCase())) : card
@@ -238,7 +317,8 @@ const Home = () => {
                 </div>
               </div>
            
-              <button type='submit' className="btn btn-primary" onClick={handleUploadDocs}>{uploadTrue ? <span className={`${uploadTrue ? "blink_me" : " "}`}>"Uploading...."</span> : "Upload Doc"}</button>
+              <button type='submit' className="btn btn-primary" onClick={handleUploadDocs}>{uploadTrue ? <span className={`${uploadTrue ? "blink_me" : " "}`}>Uploading....</span> : "Upload Doc"}</button>
+              <button type='button' className="btn btn-primary" onClick={handleDeleteDocs}> Delete Docs</button>
             </div>
           </form>
 
@@ -300,8 +380,16 @@ const Home = () => {
 
         </div>
         <div className='message-outer'>
-          <input type='text' className='form-control' placeholder='Enter a question....' value={inputMessage} onChange={(e) => setInputMessage(e.target.value)}></input>
-          <button type='submit' className='btn btn-primary' onClick={handleSend}>Send</button>
+        <button type="submit" className={`filter_btn ${message.length == 0 ? "disable_Button" : ""}`} disabled={message.length == 0} onClick={handleDeleteHistory} >
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                  <path d="M3 6H5H21" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+                  <path d="M19 6V20C19 20.5304 18.7893 21.0391 18.4142 21.4142C18.0391 21.7893 17.5304 22 17 22H7C6.46957 22 5.96086 21.7893 5.58579 21.4142C5.21071 21.0391 5 20.5304 5 20V6M8 6V4C8 3.46957 8.21071 2.96086 8.58579 2.58579C8.96086 2.21071 9.46957 2 10 2H14C14.5304 2 15.0391 2.21071 15.4142 2.58579C15.7893 2.96086 16 3.46957 16 4V6" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+                  <path d="M10 11V17" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+                  <path d="M14 11V17" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+                </svg>
+              </button>
+          <input type='text' className='form-control' disabled={showResponse} placeholder={showResponse ? 'Waiting for response....' : 'Enter a question....'} value={inputMessage} onChange={(e) => setInputMessage(e.target.value)} onKeyPress={handleKeyPress}></input>
+          <button type='submit' className='btn btn-primary' disabled={showResponse || !inputMessage} onClick={handleSend}>Send</button>
         </div>
       </div>
           
