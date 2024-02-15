a44cf3830ea0af4b1ba416ee7d8f3d625841ee8b
Panda0196 <mike.wazowski0196@gmail.com>
2024-02-14 19:51:14 -0800

    ui: make to large edit card dlg close button

--- frontend/src/components/home/Home.jsx
+++ frontend/src/components/home/Home.jsx
@@ -354,7 +354,7 @@ const Home = () => {
               <div class="modal-dialog" style={{ textAlign: 'left' }}>
                 <div class="modal-content">
                   <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    <button type="button" class="close" data-dismiss="modal" style={{fontSize: '40px'}}>&times;</button>
                     <h3 class="modal-title">Fact card</h3>
                   </div>
                   <div class="modal-body">
