edd748e409a32c579585f8599ae5b4ec10623b1d
Panda0196 <mike.wazowski0196@gmail.com>
2024-02-15 23:03:02 -0800

    fix: deploy script bug

--- backend/restart.sh
+++ backend/restart.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
-pm2 stop app
-pm2 start app.js
+
+# sudo systemctl restart mongod
+
+pm2 stop faq-chat-backend
+pm2 start app.js --name "faq-chat-backend" --watch --ignore-watch="node_modules, uploads" --log-date-format="YYYY-MM-DD HH:mm Z"
 
 pm2 list
 pm2 logs
--- frontend/restart.sh
+++ frontend/restart.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
 
-pm2 stop react-app
-pm2 start "npm start" --name "react-app" --watch --ignore-watch="node_modules" --log-date-format="YYYY-MM-DD HH:mm Z"
-
-pm2 list
-pm2 logs
+pm2 stop faq-chat-frontend
+pm2 start "npm start" --name "faq-chat-frontend" --watch --ignore-watch="node_modules" --log-date-format="YYYY-MM-DD HH:mm Z"
