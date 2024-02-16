03efc83371f08eab0c121a391f5ff3ebf2a0d90d
Panda0196 <mike.wazowski0196@gmail.com>
2024-02-15 23:57:48 -0800

    fix: write file bug in backend

--- backend/controller/cardController.js
+++ backend/controller/cardController.js
@@ -1,48 +1,53 @@
-import fs from 'fs'
+import fs from 'fs/promises'
 import path from 'path'
 import process from 'process'
 import Card from '../models/card.js'
 import { ingest } from '../utils/pinecone.js'
-
 
 const getCards = async (req, res) => {
     try {
         const cards = await Card.find()
         res.status(200).json({ sucess: true, cards: cards })
     } catch (error) {
-        res.status(500).json({ sucess: false, error: error })
+        res.status(500).json({ error: error })
     }
 }
 
 const updateCard = async (req, res) => {
-    // update db
-    const { answer, question, orgAnswer, orgQuestion } = req.body
-    const filter = { question: orgQuestion, answer: orgAnswer }
-    const data = { question: question, answer: answer }
+    try {
+        console.log("Input to updateCard:", req.body)
 
-    if (orgQuestion != '') {
-        await Card.updateOne(filter, data)
-    } else {
-        const update = new Card(data)
-        await update.save()
-    }
+        // update db
+        const { answer, question, orgAnswer, orgQuestion } = req.body
+        const filter = { question: orgQuestion, answer: orgAnswer }
+        const data = { question: question, answer: answer }
 
-    // rewrite faq.txt with all FAQs
-    const filePath = path.join(process.cwd(), 'uploads' + path.sep + 'faq.txt')
-    const fileData = await Card.find({}, '-_id question answer').exec()
-    let fileStr = ''
-    for (const e in fileData) {
-        fileStr += 'Question: ' + fileData[e]['question'] + '\n'
-        fileStr += 'Answer: ' + fileData[e]['answer'] + '\n\n'
-    }
+        if (orgQuestion != '') {
+            await Card.updateOne(filter, data)
+            console.log("Updated a fact card")
+        } else {
+            const update = new Card(data)
+            await update.save()
+            console.log("Added a fact card")
+        }
 
-    fs.writeFile(filePath, fileStr, async (err) => {
-        if (err) throw err
+        // rewrite faq.txt with all FAQs
+        const filePath = path.join(process.cwd(), 'uploads' + path.sep + 'faq.txt')
+        const fileData = await Card.find({}, '-_id question answer').exec()
+        let fileStr = ''
+        for (const e in fileData) {
+            fileStr += 'Question: ' + fileData[e]['question'] + '\n'
+            fileStr += 'Answer: ' + fileData[e]['answer'] + '\n\n'
+        }
+        await fs.writeFile(filePath, fileStr)
+        console.log("Updated the FAQ DB:", filePath)
 
         // ingest pinecone
         await ingest()
-        res.status(200).json({ success: true, message: 'OK' })
-    });
+        res.status(200).json({ success: true })
+    } catch (error) {
+        res.status(500).json({ error: error })
+    }
 }
 
 
